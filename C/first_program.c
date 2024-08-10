6cef53700dc5684947f7d1318f8357b9e6194ea6
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-08-10 16:08:39 +0530

    First C Program

---
+++ C/first_program.c
@@ -0,0 +1,19 @@
+// Online C compiler to run C program online
+#include <stdio.h>    //Header file
+
+int main() //Function Starting point 
+{
+   //Variable .... vary during run time
+   //Data Type.....Type of data we are assigning
+   
+   //Alphabet,_underscore,
+   //first number (u cannot give space) firstNumber/first_Number/FirstNumber
+   //number1(Applicable)  , 1Number(Not applicable)
+   
+   //User defined  & System Defined
+   
+   int number = 10;
+   printf("Hello Yaseen, welcome to MKPITS %d",number);
+   
+    return 0;
+}

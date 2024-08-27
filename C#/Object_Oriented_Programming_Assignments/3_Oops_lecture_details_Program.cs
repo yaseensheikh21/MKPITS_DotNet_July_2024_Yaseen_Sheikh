6cdbe87ebb729709a14bd5f634ce743dd6afa8d7
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-08-27 10:31:16 +0530

    3_Oops_lecture_details_Program.cs assignment in c# program of OOPS

---
+++ C#/Object_Oriented_Programming_Assignments/3_Oops_lecture_details_Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_s_3_lecture_details
+{
+    class Lecturedetails
+    {
+        string LectureNmae;
+        string SubjectName;
+        string CourseName;
+        int NumberofLectures;
+
+        public void AssignValues(string LectureName, string SubjectName, string CourseName, int NumberofLectures)
+        {
+            this.LectureNmae = LectureName;
+            this.SubjectName = SubjectName;
+            this.CourseName = CourseName;
+            this.NumberofLectures = NumberofLectures;
+
+        }
+        public void AddLectuerdetails()
+        {
+            Console.WriteLine(" ");
+        }
+        public void ShowValues()
+        {
+            Console.WriteLine("LectureName :" +LectureNmae);
+            Console.WriteLine("SubjectName :" +SubjectName);
+            Console.WriteLine("CourseName :" +CourseName);
+            Console.WriteLine("NumberofLectures :" + NumberofLectures);
+        }     
+       static void Main(string[] args)
+       {
+         Lecturedetails obj = new Lecturedetails();
+         obj.AssignValues("DNA", "Human body", "bio", 69);
+         obj.ShowValues();
+            Console.ReadLine();
+       }
+       
+    }
+}

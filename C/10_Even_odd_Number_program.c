b84a11a0c9fa19b9810bd97eb4207ddbd704f7ed
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-08-29 10:43:25 +0530

    10_Even_odd_Number_program.c assignment in c program

---
+++ C/10_Even_odd_Number_program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+  int num;
+  printf ("enter num:");
+  scanf("%d",&num);
+  
+  if (num%2==0)
+  {
+      printf("even num");
+  }
+ else 
+{
+    printf("odd num");
+}
+}

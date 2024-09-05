05d1e4b3183876da4974d376d6191b79e1062657
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-09-05 20:50:06 +0530

    13_A-F_array.c assignment in c program

---
+++ C/13_A-F_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+ {
+    char arr[6] = {'A', 'B', 'C', 'D', 'E', 'F'};
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%c\n", arr[i]);
+    }
+
+    return 0;
+}

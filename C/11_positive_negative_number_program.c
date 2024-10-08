85a940b055cbf3cbd2d45dff8ed3fdf7ad9cd90a
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-08 23:24:55 +0530

    11_positive_negative_number_program.c assignment in c

---
+++ C/11_positive_negative_number_program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+int num;
+printf("enter num:");
+scanf("%d",&num);
+if (num >=0)
+{
+    printf("number is positive ");
+}
+else if(num <=0)
+{
+    printf("num is negative");
+}
+    
+}

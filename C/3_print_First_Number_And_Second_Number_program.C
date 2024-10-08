b11c8ef38c7e96051f775f704ed47a607db0cffa
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-08 23:16:15 +0530

    3_print_First_Number_And_Second_Number_program.C assingment in c

---
+++ C/3_print_First_Number_And_Second_Number_program.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+  int first,second;
+  printf("enter 2 number");
+  scanf("%d",&first);
+  scanf("%d",&second);
+  printf("First number = %d\n",first);
+  printf("Second number = %d",second);
+}

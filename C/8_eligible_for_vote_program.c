668de91e358714dfafeb4f2af30e3354c60dd6d0
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-08 23:21:44 +0530

    8_eligible_for_vote_program.c assignment in c

---
+++ C/8_eligible_for_vote_program.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+void main()
+{
+    int age = 18;
+    
+    
+    printf("enter you age:");
+    scanf("%d",&age);
+    
+    if (age>=18)
+    {
+    printf("age is greater then 18 is eligible for vote ");
+    }
+    else if (age<=18)
+    {
+    printf("age is less then 18 is not eligible for vote");
+    }
+}

765138de25d7481e2329734c8f8f03995e61d07b
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-08 22:46:54 +0530

    2_print_biodata_with_help_of_program.C assignment in c program

---
+++ C/2_print_biodata_with_help_of_program.C
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+void main()
+{
+  int first,second;
+  printf("enter 2 number");
+  scanf("%d",&first);
+  scanf("%d",&second);
+  printf("First number = %d\n",first);
+  printf("Second number = %d",second);
+}

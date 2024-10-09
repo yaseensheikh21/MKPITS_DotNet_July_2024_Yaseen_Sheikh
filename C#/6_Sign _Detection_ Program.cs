b6638b70292d2ff2f0060cb56258eaca48c7d7eb
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 22:16:09 +0530

    6_Sign _Detection_ Program.cs assing in C#

---
+++ C#/6_Sign _Detection_ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Sign__Detection__Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter the value of m: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int n;
+
+
+            if (m > 0)
+            {
+                n = 1;
+            }
+            else if (m == 0)
+            {
+                n = 0;
+            }
+            else
+            {
+                n = -1;
+            }
+            Console.WriteLine("The value of n is: " + n);
+            Console.Read();
+        }
+    }
+}

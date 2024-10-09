49d9c11abe588ec12c798b8daa52b44b5b3ff1b8
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 22:13:54 +0530

    4_Showing_Given_Number_Positive _or_negative.cs assignment in C#

---
+++ C#/4_Showing_Given_Number_Positive _or_negative.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Showing_Given_Number_Positive__or_negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+
+
+            if (number > 0)
+            {
+                Console.WriteLine("The number is positive.");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative.");
+            }
+            else
+            {
+                Console.WriteLine("The number is zero.");
+            }
+            Console.Read();
+        }
+    }
+}

a77dcdc4e59dbc58370461381011fabd4a46af76
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 16:19:14 +0530

    2.Remove Elements from a List.cs assignment of List using C#

---
+++ C#/Lists/2.Remove Elements from a List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Remove_Elements_from_a_List
+{
+    internal class Program
+    {
+        static void RemoveElementFromList(List<string> items, string elementToRemove)
+        {
+            items.Remove(elementToRemove);
+
+            foreach (string item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> myItems = new List<string> { "apple", "banana", "orange", "grape" };
+            string elementToRemove = "grape";
+
+            RemoveElementFromList(myItems, elementToRemove);
+            Console.ReadLine();
+        }
+    }
+}

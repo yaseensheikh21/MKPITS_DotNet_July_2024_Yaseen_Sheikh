ea36aae433b71c1b7314763536ad7aa5c957ca66
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 22:11:56 +0530

    3_Showing_EVEN_or_ODD_Number.cs assignment in C#

---
+++ C#/3_Showing_EVEN_or_ODD_Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Showing_EVEN_or_ODD_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+            Console.Read();
+        }
+    }
+}

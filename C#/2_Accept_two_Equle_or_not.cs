98b49c6d3d842eaa6e5029f3707240264c1e7fe3
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 22:10:29 +0530

    2_Accept_two_Equle_or_not.cs assignment in C#

---
+++ C#/2_Accept_two_Equle_or_not.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Accept_two_Equle_or_not
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second integer: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("The numbers are equal.");
+            }
+            else
+            {
+                Console.WriteLine("The numbers are not equal.");
+            }
+            Console.Read();
+        }
+    }
+}

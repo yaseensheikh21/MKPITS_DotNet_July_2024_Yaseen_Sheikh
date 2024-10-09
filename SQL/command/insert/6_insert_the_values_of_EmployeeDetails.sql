782a6e76aec434fa55cc996c005b691808de89bd
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 12:54:14 +0530

    6_insert_the_values_of_EmployeeDetails.sql using insert command

---
+++ SQL/command/insert/6_insert_the_values_of_EmployeeDetails.sql
@@ -0,0 +1,9 @@
+insert into EmployeeDetails (
+EmpName,Department,EmpSalary,EmpContact,EmpAddress)
+values
+('mahesh',65,5888,96858555,'warora'),
+('yash',77,7000,879654213,'puna'),
+('asif',75,8078,874527825,'jabalpur'),
+('adity',45,4097,789356421,'chandrapur')
+
+insert into EmployeeDetails (EmpName,EmpAge) values ('khan',20),('suraj',19),('basim',25)

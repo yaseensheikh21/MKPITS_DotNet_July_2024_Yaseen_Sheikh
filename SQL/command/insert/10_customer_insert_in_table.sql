3e26848f7b75f7dfd2a1e4d45352c5f96308260e
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 22:50:34 +0530

    10_customer_insert_in_table.sql insert in customer

---
+++ SQL/command/insert/10_customer_insert_in_table.sql
@@ -0,0 +1,11 @@
+SET IDENTITY_INSERT Customer oN
+INSERT INTO[Customer]([Id],[FirstName],[LastName],[City],[Country],[Phone])
+VALUES (94, 'saniay', 'khan','katmandu','nepal','555-4577-878')
+INSERT INTO [Customer] ([Id], [FirstName], [LastName],[City],[Country],[Phone])
+VALUES (96, 'orang', 'weight','lahor','paksitan','555545-84-66')
+INSERT INTO [Customer] ([Id], [FirstName], [LastName],[City],[Country],[Phone])
+VALUES (97, 'ninja', 'h2','Lucknow','india','77452-89654')
+INSERT INTO [Customer] ([Id], [FirstName], [LastName],[City],[Country],[Phone])
+VALUES (98, 'bmw', 'car','UK','USA','4855454-5555')
+INSERT INTO [Customer] ([Id], [FirstName], [LastName],[City],[Country],[Phone])
+VALUES (99, 'bike', 'zx10r','nagpur','india','555-444-5555')

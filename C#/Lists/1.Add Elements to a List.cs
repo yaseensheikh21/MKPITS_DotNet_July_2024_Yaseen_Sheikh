eaa9ad9861159785c41b7715ea907071453099db
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-09 16:14:39 +0530

    1.Add Elements to a List.cs using C# assignment of List

---
+++ C#/Lists/1.Add Elements to a List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Add_Elements_to_a_List
+{
+    internal class Program
+    {
+        static void AddElementsToList(List<int> numbers, List<int> elementsToAdd)
+        {
+            numbers.AddRange(elementsToAdd);
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> myNumbers = new List<int> { 10, 20, 30 };
+            List<int> elementsToAdd = new List<int> { 1, 2, 3, 4, 5 };
+
+            AddElementsToList(myNumbers, elementsToAdd);
+            Console.Read();
+        }
+        
+    }
+}

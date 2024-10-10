f5b6e044e839eb94db8da0dcb70f611c7a6a9a5e
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-10 20:19:36 +0530

    22_ Print_next_ 5_elements .c program using whit array

---
+++ C/22_ Print_next_ 5_elements .c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[6] = {2000, 2001, 2002, 2003, 2004, 2005};
+
+    printf("%d\n", arr[0]);
+    printf("%d\n", arr[1]);
+    printf("%d\n", arr[2]);
+    printf("%d\n", arr[3]);
+    printf("%d\n", arr[4]);
+    printf("%d\n", arr[5]);
+
+    return 0;
+}

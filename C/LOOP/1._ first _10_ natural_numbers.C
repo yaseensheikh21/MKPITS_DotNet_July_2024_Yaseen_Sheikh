2af0d79351de7bfc7606fae19dfa0aa7a609b8d9
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-13 21:02:11 +0530

    1._ first _10_ natural_numbers.C assignment in LOOP

---
+++ C/LOOP/1._ first _10_ natural_numbers.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int  main() 
+{
+    printf("I went in C in simple logic:\n");
+    
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return.0;
+}

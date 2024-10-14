26d96d8ebe6b39eeb3a35574510a64fe160e04b5
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-14 21:14:04 +0530

    15_calculate_factorial_of_given_number.c assignment in c using loops

---
+++ C/LOOP/15_calculate_factorial_of_given_number.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num;
+    unsigned long long factorial = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+
+    if (num < 0) 
+    {
+        printf("Factorial of a negative number doesn't exist.\n");
+    } 
+    else 
+    {
+       
+        for (int i = 1; i <= num; i++) 
+        {
+            factorial *= i;
+        }
+
+        printf("Factorial of %d = %llu\n", num, factorial);
+    }
+
+    return 0;
+}

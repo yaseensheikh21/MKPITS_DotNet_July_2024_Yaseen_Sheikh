7cfa5749b7df38c8fa83fea40285385794300751
Yaseen Sheikh <yaseenshiekh2004@gmail.com>
2024-10-22 11:08:11 +0530

    add untraced file

---
+++ C/24_SUM_1ST_ AND_ 5TH_ELEMENT_Array.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() 
+{
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int sum = arr[0] + arr[4];
+    printf("Sum of 1st and 5th elements: %d\n", sum);
+    return 0;
+}
--- C/2_print_biodata_with_help_of_program.C
+++ C/2_print_biodata_with_help_of_program.C
@@ -1,11 +1,27 @@
-
 #include <stdio.h>
 void main()
 {
-  int first,second;
-  printf("enter 2 number");
-  scanf("%d",&first);
-  scanf("%d",&second);
-  printf("First number = %d\n",first);
-  printf("Second number = %d",second);
-}
+    char Name[10];
+    int age;
+    char Qualification[10];
+    char Stream[6];
+    printf("enter name :");
+    printf("enter 2number: ");
+    printf("enter Qualification: ");
+    printf("enter Stream :");
+    scanf("%s",&Name);
+    scanf("%d",&age);
+    scanf("%s",&Qualification);
+    scanf("%s",&Stream);
+    printf("Name : %s\n",Name);
+    printf("age: %d\n",age);
+    printf("qualification: %s\n",Qualification);
+    printf("stream: %s\n",Stream);  
+    }
+
+
+
+
+
+
+
